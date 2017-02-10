b09b4fa96f49caf845814f05c8e8994b08459c90
Luis Fernando <luisfernandoguina@hotmail.com>
2017-02-09 22:32:57 -0500

    Install Laravel

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,2 @@
+<h1>Bienvenido con h1</h1>
+<p>Bienvenido {{$user->name}}  a Styde</p>
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Mail\Welcome;
+use App\User;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+Route::get('/welcome',function (){
+    $user = new User([
+        'name'=>'Luis Fernando',
+        'email'=>'luisfernandoguina@gmail.com',
+
+    ]);
+
+
+    Mail::to($user->email,$user->name)
+        ->send(new Welcome($user));
+});
