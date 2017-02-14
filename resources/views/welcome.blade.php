0bcab60fa57ab31fbcc383e364ef36b68c2da6b3
Luis Fernando <luisfernandoguina@hotmail.com>
2017-02-14 12:22:27 -0500

    Nuevo Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,12 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Fernando Guiña es la ley.
                 </div>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laracasts.com">Debe ir Styde</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
