ceaa7dc9a27f283e7ad50dc029e445108563c833
Luis Fernando <luisfernandoguina@hotmail.com>
2017-02-14 13:10:41 -0500

    Nuevo fernando en misma linea

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Fernando cambiando la misma linea jej
                 </div>
 
                 <div class="links">
